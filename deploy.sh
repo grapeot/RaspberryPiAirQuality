0e582985a7f09729bb5532ec7385d7ba9ba3c635
Yan Wang <grapeot@gmail.com>
2017-01-07 16:34:47 -0800

    Update the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 # install python dependencies
 sudo apt-get update
-sudo apt-get install python3-dev python3-pip swig2.0
-pip3 install spidev rpi.gpio
+sudo apt-get install -y python3-dev python3-pip swig2.0
+sudo pip3 install spidev rpi.gpio
 # Compile and install the wiringpi library, detailed instructions available at https://github.com/WiringPi/WiringPi-Python
-git clone https://github.com/WiringPi/WiringPi-Python
-cd WiringPi
+git clone https://github.com/WiringPi/WiringPi-Python --recursive
+cd WiringPi-Python
 swig2.0 -python -threads wiringpi.i
 sudo python3 setup.py build install
 cd ..
 
 # install nodejs + pm2 to launch
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+source ~/.bashrc
 nvm install node
 npm install -g pm2
-
